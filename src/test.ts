d7cfbcce001a7fbfd12ff1fab5ddfd8d2733dc54
Saeed Sheikh <saeedsaddiqui99@gmail.com>
2024-04-18 16:49:39 +0500

    Firstly Push Code

---
+++ src/test.ts
@@ -0,0 +1,36 @@
+#! /usr/bin/env node
+
+import inquirer from "inquirer";
+
+const answers = await inquirer.prompt([
+  {
+    name: "firstNumber",
+    type: "number",
+    message: "Enter your first number",
+  },
+  {
+    name: "secondNumber",
+    type: "number",
+    message: "Enter your second number", 
+  },
+  {
+    name: "Operator",
+    type: "list",
+    message: "Select Operation",
+    choices: ["Addition", "Subtraction", "Multiplication", "Division"],
+  },
+]);
+
+// console.log(answers)
+
+if (answers.Operator === "Addition") {
+  console.log(answers.firstNumber + answers.secondNumber);
+} else if (answers.Operator === "Subtraction") {
+  console.log(answers.firstNumber - answers.secondNumber);
+} else if (answers.Operator === "Multiplication") {
+  console.log(answers.firstNumber * answers.secondNumber);
+} else if (answers.Operation === "Division") {
+  console.log(answers.firstNumber / answers.secondNumber);
+} else {
+  console.log("Kindly Enter Valid Operator ");
+}
